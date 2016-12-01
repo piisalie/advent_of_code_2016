643c608c71852e3a26893fe41540cb26a4f5a069
Paul Dawson <excidere@gmail.com>
2016-12-01 10:59:08 -0600

    the second star

--- day1/pathfinder.ex
+++
@@ -1,5 +0,0 @@
-defmodule Pathfinder do
-
-
-
-end
--- day1/pathfinder_test.ex
+++ day1/pathfinder_test.ex
@@ -2,61 +2,78 @@ ExUnit.start
 
 defmodule Pathfinder do
 
-  def move(heroine, "L" <> amount), do: move_left(heroine, amount)
-  def move(heroine, "R" <> amount), do: move_right(heroine, amount)
-
-  def compute_distance({_movements, heroine}) do
-    abs(heroine.x) + abs(heroine.y)
+  def move(heroine, "L" <> amount) do
+    heroine
+    |> turn_left
+    |> walk(String.to_integer(amount))
+  end
+  def move(heroine, "R" <> amount) do
+    heroine
+    |> turn_right
+    |> walk(String.to_integer(amount))
   end
 
-  defp move_left(%{x: x, y: y, heading: :north}, count) do
-    %{x: x - String.to_integer(count),
-      y: y,
-      heading: :west
-    }
+  def compute_distance(%{duplicate: duplicate} = heroine) do
+    %{total_distance: abs(heroine.x) + abs(heroine.y),
+      distance_to_duplicate_location: abs(elem(duplicate, 0)) + abs(elem(duplicate, 1))}
   end
-  defp move_left(%{x: x, y: y, heading: :south}, count) do
-    %{x: x + String.to_integer(count),
-      y: y,
-      heading: :east
-    }
-  end
-  defp move_left(%{x: x, y: y, heading: :east}, count) do
-    %{x: x,
-      y: y + String.to_integer(count),
-      heading: :north
-    }
-  end
-  defp move_left(%{x: x, y: y, heading: :west}, count) do
-    %{x: x,
-      y: y - String.to_integer(count),
-      heading: :south
-    }
+  def compute_distance(heroine) do
+    %{total_distance: abs(heroine.x) + abs(heroine.y)}
   end
 
-  defp move_right(%{x: x, y: y, heading: :north}, count) do
-    %{x: x + String.to_integer(count),
-      y: y,
-      heading: :east
-    }
+  defp turn_left(%{heading: :north} = heroine) do
+    %{heroine | heading: :west}
   end
-  defp move_right(%{x: x, y: y, heading: :south}, count) do
-    %{x: x - String.to_integer(count),
-      y: y,
-      heading: :west
-    }
+  defp turn_left(%{heading: :south} = heroine) do
+    %{heroine | heading: :east}
   end
-  defp move_right(%{x: x, y: y, heading: :east}, count) do
-    %{x: x,
-      y: y - String.to_integer(count),
-      heading: :south
-    }
+  defp turn_left(%{heading: :east} = heroine) do
+    %{heroine | heading: :north}
   end
-  defp move_right(%{x: x, y: y, heading: :west}, count) do
-    %{x: x,
-      y: y + String.to_integer(count),
-      heading: :north
-    }
+  defp turn_left(%{heading: :west} = heroine) do
+    %{heroine | heading: :south}
+  end
+
+  defp turn_right(%{heading: :north} = heroine) do
+    %{heroine | heading: :east}
+  end
+  defp turn_right(%{heading: :south} = heroine) do
+    %{heroine | heading: :west}
+  end
+  defp turn_right(%{heading: :east} = heroine) do
+    %{heroine | heading: :south}
+  end
+  defp turn_right(%{heading: :west} = heroine) do
+    %{heroine | heading: :north}
+  end
+
+  defp walk(heroine, 0), do: heroine
+  defp walk(%{x: x, y: y, heading: :north} = heroine, amount) do
+    %{heroine | y: y + 1, history: [{x,y} | heroine.history]}
+    |> check_duplicates
+    |> walk(amount - 1)
+  end
+  defp walk(%{x: x, y: y, heading: :south} = heroine, amount) do
+    %{heroine | y: y - 1, history: [{x,y} | heroine.history]}
+    |> check_duplicates
+    |> walk(amount - 1)
+  end
+  defp walk(%{x: x, y: y, heading: :east} = heroine, amount) do
+    %{heroine | x: x + 1, history: [{x,y} | heroine.history]}
+    |> check_duplicates
+    |> walk(amount - 1)
+  end
+  defp walk(%{x: x, y: y, heading: :west} = heroine, amount) do
+    %{heroine | x: x - 1, history: [{x,y} | heroine.history]}
+    |> check_duplicates
+    |> walk(amount - 1)
+  end
+
+  defp check_duplicates(%{x: x, y: y, history: history} = heroine) do
+    case Enum.find(history, fn (coord) -> coord == {x,y} end) do
+      nil -> heroine
+      duplicate -> Map.put_new(heroine, :duplicate, duplicate)
+    end
   end
 
 end
@@ -65,7 +82,7 @@ defmodule PathfinderTest do
   use ExUnit.Case, async: true
 
   test "can move a heroine left when heading north" do
-    fearless_heroine = %{x: 0, y: 0, heading: :north}
+    fearless_heroine = %{x: 0, y: 0, heading: :north, history: []}
     |> Pathfinder.move("L2")
 
     assert fearless_heroine.x == -2
@@ -74,7 +91,7 @@ defmodule PathfinderTest do
   end
 
   test "can move a heroine left when heading south" do
-    fearless_heroine = %{x: 0, y: 0, heading: :south}
+    fearless_heroine = %{x: 0, y: 0, heading: :south, history: []}
     |> Pathfinder.move("L2")
 
     assert fearless_heroine.x == 2
@@ -83,7 +100,7 @@ defmodule PathfinderTest do
   end
 
   test "can move a heroine left when heading east" do
-    fearless_heroine = %{x: 0, y: 0, heading: :east}
+    fearless_heroine = %{x: 0, y: 0, heading: :east, history: []}
     |> Pathfinder.move("L2")
 
     assert fearless_heroine.x == 0
@@ -92,7 +109,7 @@ defmodule PathfinderTest do
   end
 
   test "can move a heroine left when heading west" do
-    fearless_heroine = %{x: 0, y: 0, heading: :west}
+    fearless_heroine = %{x: 0, y: 0, heading: :west, history: []}
     |> Pathfinder.move("L2")
 
     assert fearless_heroine.x == 0
@@ -101,7 +118,7 @@ defmodule PathfinderTest do
   end
 
   test "can move a heroine right when heading north" do
-    fearless_heroine = %{x: 0, y: 0, heading: :north}
+    fearless_heroine = %{x: 0, y: 0, heading: :north, history: []}
     |> Pathfinder.move("R2")
 
     assert fearless_heroine.x == 2
@@ -110,7 +127,7 @@ defmodule PathfinderTest do
   end
 
   test "can move a heroine right when heading south" do
-    fearless_heroine = %{x: 0, y: 0, heading: :south}
+    fearless_heroine = %{x: 0, y: 0, heading: :south, history: []}
     |> Pathfinder.move("R2")
 
     assert fearless_heroine.x == -2
@@ -119,7 +136,7 @@ defmodule PathfinderTest do
   end
 
   test "can move a heroine right when heading east" do
-    fearless_heroine = %{x: 0, y: 0, heading: :east}
+    fearless_heroine = %{x: 0, y: 0, heading: :east, history: []}
     |> Pathfinder.move("R2")
 
     assert fearless_heroine.x == 0
@@ -128,7 +145,7 @@ defmodule PathfinderTest do
   end
 
   test "can move a heroine right when heading west" do
-    fearless_heroine = %{x: 0, y: 0, heading: :west}
+    fearless_heroine = %{x: 0, y: 0, heading: :west, history: []}
     |> Pathfinder.move("R2")
 
     assert fearless_heroine.x == 0
@@ -140,6 +157,7 @@ end
 
 "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3"
 |> String.split(", ")
-|> Enum.map_reduce(%{x: 0, y: 0, heading: :north}, fn (movement, heroine) -> {movement, Pathfinder.move(heroine, movement)} end)
+|> Enum.map_reduce(%{x: 0, y: 0, heading: :north, history: []}, fn (movement, heroine) -> {movement, Pathfinder.move(heroine, movement)} end)
+|> elem(1)
 |> Pathfinder.compute_distance
 |> IO.inspect
